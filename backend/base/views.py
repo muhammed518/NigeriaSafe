77e0960e92c7a8b23b5f613dd3826bfe38b6cd91
muhammed danjuma <danjumamuhammed100@gmail.com>
2025-12-15 12:10:16 +0100

    "something something models"

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Welcome to NigeriaSafe!")
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse("This is the About page of NigeriaSafe.")
+    return render(request, 'about.html') 
