92bf7fcf5fcedb1492b2ab8f0277224bb69263cb
muhammed <muhammed@gmail.com>
2025-12-19 19:48:37 +0100

    fix 2

--- backend/base/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Patient
-
-admin.site.register(Patient)
--- backend/nigeriasafe/asgi.py
+++ backend/nigeriasafe/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for nigeriasafe project.
+ASGI config for nigeriasaf project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nigeriasafe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nigeriasaf.settings')
 
 application = get_asgi_application()
